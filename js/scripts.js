1a15663978f5f7aa69ec7b3c1d7325aef405437e
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-24 18:31:12 +0200

    styled snack 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 */
 // list of vip guests
 vipList = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+// initializing data
 let place = 0
 let table = "Vip Table"
 let nameVip = ""
@@ -25,7 +26,7 @@ console.log(vipz);
 // fn to print in html every vip
 function printTicket(name,placeNum,tableType) {
     let template =
-        `
+    `
     <div class="col-12 col-lg-5 my-5 py-4 rounded-5">
         <strong>Name</strong>
         <h2>${name}</h2>
@@ -90,3 +91,30 @@ const StudentsUp70 = students.filter((student) => student.grades > 70);
 
 //todo << creation of a list with students whose grades are > 70
 const StudentsUp70ID120 = students.filter((student) => student.grades > 70 && student.studentID > 120);
+
+const row2 = document.querySelector("#snack-2 .row")
+const colName = document.getElementById("name")
+const colUp70 = document.getElementById("up70")
+const colBoth = document.getElementById("both")
+
+studentsName.forEach((st)=> {
+    colName.innerHTML +=
+    `
+    <h2>${st}</h2>
+    `
+})
+StudentsUp70.forEach((stup)=> {
+    colUp70.innerHTML +=
+    `
+    <h2>${stup.studentName}</h2>
+    <h2>${stup.grades}</h2>
+    `
+
+})
+StudentsUp70ID120.forEach((stbo)=> {
+    colBoth.innerHTML +=
+    `
+    <h2>Grade : ${stbo.grades}</h2>
+    <h2>ID : ${stbo.studentID}</h2>
+    `
+})
