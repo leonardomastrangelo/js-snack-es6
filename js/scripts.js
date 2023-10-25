ea6d2b853bb749fd99c69423c28a6af03655b5f3
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-25 16:05:25 +0200

    styled snack-3

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ const vipz = vipList.map((vip, index) => {
     person.nameVip = vip
     person.placeNum = index + 1
     person.table = table
-    printTicket(person.nameVip,person.placeNum,person.table)
+    printTicket(person.nameVip, person.placeNum, person.table)
     return person
 }
 )
@@ -24,9 +24,9 @@ const vipz = vipList.map((vip, index) => {
 console.log(vipz);
 
 // fn to print in html every vip
-function printTicket(name,placeNum,tableType) {
+function printTicket(name, placeNum, tableType) {
     let template =
-    `
+        `
     <div class="col-12 col-lg-5 my-5 py-4 rounded-5">
         <strong>Name</strong>
         <h2>${name}</h2>
@@ -96,22 +96,22 @@ const colName = document.getElementById("name")
 const colUp70 = document.getElementById("up70")
 const colBoth = document.getElementById("both")
 
-studentsName.forEach((st)=> {
+studentsName.forEach((st) => {
     colName.innerHTML +=
-    `
+        `
     <h2>${st}</h2>
     `
 })
-StudentsUp70.forEach((stup)=> {
+StudentsUp70.forEach((stup) => {
     colUp70.innerHTML +=
-    `
+        `
     <h2>${stup.studentName}</h2>
     <h2>Grade : ${stup.grades}</h2>
     `
 })
-StudentsUp70ID120.forEach((stbo)=> {
+StudentsUp70ID120.forEach((stbo) => {
     colBoth.innerHTML +=
-    `
+        `
     <h2>Grade : ${stbo.grades}</h2>
     <h2>ID : ${stbo.studentID}</h2>
     `
@@ -120,3 +120,77 @@ StudentsUp70ID120.forEach((stbo)=> {
 /**
  * * SNACK 3 *
 */
+//todo << Create an array of objects with proprerties-> "name", "weight"
+const cycles = 
+[
+    {
+        nome : "pippo",
+        weight : "27"
+    },
+    {
+        nome : "paperino",
+        weight : "50"
+    },
+    {
+        nome : "pluto",
+        weight : "24"
+    },
+    {
+        nome : "topolino",
+        weight : "74"
+    },
+    {
+        nome : "topolino",
+        weight : "10"
+    },
+    {
+        nome : "minnie",
+        weight : "46"
+    },
+    {
+        nome : "tony",
+        weight : "67"
+    },
+    {
+        nome : "mario",
+        weight : "89"
+    },
+    {
+        nome : "giuseppe",
+        weight : "14"
+    },
+]
+
+// take the lowest weight value
+const {nome,weight} = cycles[4]
+// print he lowest weight value in console
+console.log(nome,weight);
+// .row from html
+const row3 = document.querySelector("#snack-3 .row");
+// printing lowest weight value in html
+printCycle()
+function printCycle() {
+    let template = 
+    `
+    <div class="col-5">
+        <h2 class="display-1 mb-5">Cycle with lowest weight</h2>
+        <div class="bg-light rounded-top-3 pt-3">
+            <span class="display-5">Cycle : </span>
+            <span class="display-3">${nome}</span>
+        </div> 
+        <div class="bg-light rounded-bottom-3 pb-3">
+            <span class="display-5">Weight : </span>
+            <span class="display-3">${weight}kg</span>
+        </div> 
+    </div>
+    `
+    row3.innerHTML += template
+}
+
+
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
